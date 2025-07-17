0dd4d8515e98e8dffea31e1f461f6c0963336e6d
Pranjal Maharaniya <pj3511044@gmail.com>
2025-07-17 14:49:48 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
       <Box display="flex" justifyContent="center" alignItems="center" gap={1}>
         <img src={pjLogo} alt="PJ Logo" width="18" height="18" style={{ borderRadius: '10px' }} />
         <Typography variant="body 5" sx={{ fontWeight: 100 }}>
-          © 2025 <strong>PJ</strong>. All rights reserved.
+          2025 <strong>PJ</strong>
         </Typography>
       </Box>
     </Box>
